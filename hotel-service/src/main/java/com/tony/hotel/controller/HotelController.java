17c131ee09bf68a087e72a94806ae49a9c77bf95
huyf <724826175@qq.com>
2023-04-03 11:53:15 +0800

    添加RestHighLevelClient 创建索引逻辑

--- hotel-service/src/main/java/com/tony/hotel/controller/HotelController.java
+++ hotel-service/src/main/java/com/tony/hotel/controller/HotelController.java
@@ -1,12 +1,20 @@
 package com.tony.hotel.controller;
 
+import com.tony.hotel.constants.HotelConstants;
 import com.tony.hotel.domain.Hotel;
 import com.tony.hotel.service.HotelService;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.client.indices.CreateIndexRequest;
+import org.elasticsearch.common.xcontent.XContentType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -17,9 +25,20 @@ public class HotelController {
     private HotelService hotelService;
 
     @GetMapping
-    public List<Hotel> getAll() {
-        List<Hotel> list = hotelService.list();
-        return list;
+    public String getAll() throws IOException {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(HttpHost.create("http://localhost:9200")));
+
+        CreateIndexRequest request = new CreateIndexRequest("hotel");
+
+        request.source(HotelConstants.MAPPING_TEMPLATE, XContentType.JSON);
+
+        client.indices().create(request, RequestOptions.DEFAULT);
+
+//        List<Hotel> list = hotelService.list();
+//        return list;
+
+        return "success";
     }
 
     @GetMapping("/one")
